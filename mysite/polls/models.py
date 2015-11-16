2f0c2b8e57ad46e1535499d925e944c59a876bb1
Sabaychitra84 <sabaychitra84@gmail.com>
2015-11-16 16:59:14 +0700

    Add some files and changed somethings

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -2,9 +2,15 @@ from django.db import models
 
 class Question(models.Model):
 	question_text = models.CharField(max_length=200)
-	pub_date = models.DateTimeField()
+	pub_date = models.DateTimeField("Date Published")
+
+	def __str__(self):
+		return self.question_text
 
 class Choice(models.Model):
 	question = models.ForeignKey(Question, on_delete=models.CASCADE)
 	choice_text = models.CharField(max_length=100)
 	vote = models.IntegerField(default=0)
+
+	def __str__(self):
+		return self.question_text
