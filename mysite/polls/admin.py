d4040ae8775c8a5dfab00ed0e96de708776f5774
Chitra <sabaychitra84@gmail.com>
2015-11-16 22:57:30 +0700

    Tonight

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from . models import Question, Choice
 
-# Register your models here.
 
-admin.site.register(Question)
+class ChoiceInline(models.stackInline):
+	extra = 1
+	choices = Choice
+
+class QuestionAdmin(admin.ModelAdmin):
+	list_display = ("question_text","pub_date")
+	inlines = ChoiceInline
+admin.site.register(Question, QuestionAdmin)
 
 
