e8e46018c84506c940bc0d0bb43cf63a789c9d6d
Sabaychitra84 <sabaychitra84@gmail.com>
2015-11-18 10:56:30 +0700

    Last workd

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,13 +2,14 @@ from django.contrib import admin
 from . models import Question, Choice
 
 
-class ChoiceInline(models.stackInline):
-	extra = 1
-	choices = Choice
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
-	list_display = ("question_text","pub_date")
-	inlines = ChoiceInline
+	list_display = ("question_text","pub_date","was_published_recently")
+	inlines  = [ChoiceInline]
+
 admin.site.register(Question, QuestionAdmin)
 
 
